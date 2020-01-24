2a28b7bc59045d2056aead18ac1ac9c1bc372788
budikpet <sgt.petrov@gmail.com>
2020-01-24 10:53:33 +0100

    Now able to use Slack only if proper config is provided.

--- package_algorithms/__init__.py
+++ package_algorithms/__init__.py
@@ -1,3 +1,4 @@
+import os
 from package_algorithms.knapsack.bb import BranchBound, SortedBranchBound
 from package_algorithms.knapsack.dp import DynamicProgramming, DynamicProgramming_Weight
 from package_algorithms.knapsack.basic import BruteForce, Greedy
@@ -7,6 +8,8 @@ from package_algorithms.knapsack.sa_old import SimulatedAnnealing_OLD
 from package_algorithms.sat.sa_sat_v1 import SimulatedAnnealing_SAT_V1
 from package_algorithms.sat.sa_sat_v2 import SimulatedAnnealing_SAT_V2
 from package_algorithms.sat.sa_sat_v3 import SimulatedAnnealing_SAT_V3
+
+os.environ["slack_config"] = "configs/slack_communicator.cfg"
 
 # __plugins__ = [BranchBound, BruteForce, DynamicProgramming, DynamicProgramming_Weight, Greedy, SortedBranchBound, SimulatedAnnealing, SimulatedAnnealingPenalty, SimulatedAnnealing_OLD]
 __plugins__  = [SimulatedAnnealing_SAT_V1, SimulatedAnnealing_SAT_V2, SimulatedAnnealing_SAT_V3]
